4573b5aa510e4ed82ba856213c4a77e345eb2596
PedroAnt628 <pemorecarr@gmail.com>
2026-03-21 12:35:48 +0100

    aaa

---
+++ practicaExamen/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900">
+      <h1 className="bg-orange-500 text-white p-10 text-5xl font-black rounded-full rotate-3 shadow-2xl">
+        TAILWIND OK ✅
+      </h1>
+    </div>
+  )
+}
+export default App
